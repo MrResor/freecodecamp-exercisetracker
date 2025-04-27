b30904b8f73eb73fb293336a11f6b52804b66b7f
Mr.Resor <jakusienko@gmail.com>
2025-04-27 17:05:39 +0000

    change _ID type to string and conform remaining tables

--- setup/db-init.sh
+++ setup/db-init.sh
@@ -2,9 +2,10 @@
 
 psql -U postgres -c "CREATE USER $PGUSER WITH PASSWORD '$PGPASSWORD'"
 psql -U postgres -c "CREATE DATABASE exercise_tracker WITH OWNER = $PGUSER"
-psql -U postgres -d exercise_tracker -c "CREATE TABLE USERS (_ID SERIAL PRIMARY KEY, username TEXT NOT NULL)" 
+psql -U postgres -d exercise_tracker -c "CREATE SEQUENCE tag_seq START WITH 1"
+psql -U postgres -d exercise_tracker -c "CREATE TABLE USERS (_ID TEXT PRIMARY KEY NOT NULL DEFAULT 'id_'||nextval('tag_seq'::regclass), username TEXT NOT NULL UNIQUE)" 
 psql -U postgres -d exercise_tracker -c "GRANT SELECT, INSERT, UPDATE, DELETE ON USERS TO $PGUSER"
-psql -U postgres -d exercise_tracker -c "GRANT USAGE, SELECT ON SEQUENCE USERS__id_seq TO $PGUSER"
-psql -U postgres -d exercise_tracker -c "CREATE TABLE EXERCISES (_ID SERIAL PRIMARY KEY, description TEXT NOT NULL, duration INT NOT NULL, date DATE NOT NULL, user_id INT REFERENCES USERS(_ID))"
+psql -U postgres -d exercise_tracker -c "GRANT USAGE, SELECT ON SEQUENCE tag_seq TO $PGUSER"
+psql -U postgres -d exercise_tracker -c "CREATE TABLE EXERCISES (_ID SERIAL PRIMARY KEY, description TEXT NOT NULL, duration INT NOT NULL, date DATE NOT NULL, user_id TEXT REFERENCES USERS(_ID))"
 psql -U postgres -d exercise_tracker -c "GRANT SELECT, INSERT, UPDATE, DELETE ON EXERCISES TO $PGUSER"
 psql -U postgres -d exercise_tracker -c "GRANT USAGE, SELECT ON SEQUENCE EXERCISES__id_seq TO $PGUSER"
