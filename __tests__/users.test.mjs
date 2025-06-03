b1bc1cea8b67cd098d7c25bcad04eed3db2c24c2
Mr.Resor <jakusienko@gmail.com>
2025-06-03 18:49:42 +0000

    add more tests

--- __tests__/users.test.mjs
+++ __tests__/users.test.mjs
@@ -33,6 +33,8 @@ beforeAll(async () => {
 
 }, 60_000)
 
+var id_testuser, id_nextuser;
+
 describe('/api/users', () => {
   it('No users in database', async () => {
     const res = await request(app).get('/api/users')
@@ -44,6 +46,8 @@ describe('/api/users', () => {
   it('Add user', async () => {
     const res = await request(app).post('/api/users').send({ username: 'testuser' })
 
+    id_testuser = res.body._id
+
     expect(res.statusCode).toBe(201)
     expect(res.body).toEqual({
       _id: expect.stringMatching(/id_[0-9]+$/),
@@ -54,10 +58,10 @@ describe('/api/users', () => {
   it("Get one user", async () => {
     const res = await request(app).get('/api/users')
 
-    expect(res.statusCode).toBe(201)
+    expect(res.statusCode).toBe(200)
     expect(res.body.length).toBe(1)
     expect(res.body[0]).toEqual({
-      _id: "id_1",
+      _id: id_testuser,
       username: 'testuser',
     })
   })
@@ -72,6 +76,44 @@ describe('/api/users', () => {
     })
   })
   // ADD NEXT USER
+  it('Add next user', async () => {
+    const res = await request(app).post('/api/users').send({ username: 'nextuser' })
+
+    id_nextuser = res.body._id
+
+    expect(res.statusCode).toBe(201)
+    expect(res.body).toEqual({
+      _id: expect.stringMatching(/id_[0-9]+$/),
+      username: 'nextuser',
+    })
+  })
   // GET ALL USERS
+  it('Get all users', async () => {
+    const res = await request(app).get('/api/users')
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body.length).toBe(2)
+    expect(res.body[0]).toEqual({
+      _id: id_testuser,
+      username: 'testuser',
+    })
+    expect(res.body[1]).toEqual({
+      _id: id_nextuser,
+      username: 'nextuser',
+    })
+  })
   // TRY TO ADD USER WITH EMPTY NAME
+  it('Try to add user with empty name', async () => {
+    const res = await request(app).post('/api/users').send({ username: '' })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({ error: 'Username is required' })
+  })
+  // TRY TO ADD USER WITHOUT NAME
+  it('Try to add user without name', async () => {
+    const res = await request(app).post('/api/users').send({})
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({ error: 'Username is required' })
+  })
 })
