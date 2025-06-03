e0b625229f3a11b374a5be60367e689638a54ab7
Mr.Resor <jakusienko@gmail.com>
2025-06-03 17:34:49 +0000

    add db setup to hello.test.mjs

--- __tests__/hello.test.mjs
+++ __tests__/hello.test.mjs
@@ -1,7 +1,41 @@
-import { describe, it, expect } from 'vitest'
+import 'dotenv/config';
 import request from 'supertest'
+import { GenericContainer, Wait } from 'testcontainers'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 
-import { app } from '../src/express.mjs'
+let buildtContainer, container, imageName, app
+
+beforeAll(async () => {
+  buildtContainer = await GenericContainer
+  .fromDockerfile(".", "Dockerfile_db")
+  .build()
+
+  container = await buildtContainer.withEnvironment({
+    POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+    PGUSER: process.env.USER_LOGIN,
+    PGPASSWORD: process.env.USER_PASSWORD,
+  })
+  .withExposedPorts(5432)
+  .withHealthCheck({
+    test: ["CMD-SHELL", "pg_isready -U ${PGUSER} -d exercise_tracker"],
+    interval: 10_000,
+    retries: 20,
+    start_interval: 30_000,
+    timeout: 3_000
+  })
+  .withWaitStrategy(Wait.forHealthCheck())
+  .withNetworkMode('host')
+  .start()
+  
+  imageName = container.getImageName // Save the image name
+
+  //dbport = container.getMappedPort(5432)
+
+   // Import the app AFTER the container is up and env vars are set
+  const mod = await import('../src/express.mjs');
+  app = mod.app;
+
+}, 60_000)
 
 describe('/api/hello', () => {
   it('should say hello', async () => {
