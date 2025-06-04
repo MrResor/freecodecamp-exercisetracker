ee6d4caf55903fdab99f9bcfbe0bc33494c0093f
Mr.Resor <jakusienko@gmail.com>
2025-06-04 19:46:04 +0000

    change solution attempting to stop all files to be run at once

--- vitest.config.js
+++ vitest.config.js
@@ -4,10 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
-    poolOptions: {
-      threads: {
-        singleThread: true,
-      }
-    }
+    fileParallelism: false,
   }
 })
