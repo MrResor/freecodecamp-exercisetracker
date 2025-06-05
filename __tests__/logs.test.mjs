eca77d6acb3c99e6f746ca3353ff06ef09fcaa19
Mr.Resor <jakusienko@gmail.com>
2025-06-05 19:32:31 +0000

    add /api/users/:id/logs endpoint tests

---
+++ __tests__/logs.test.mjs
@@ -0,0 +1,48 @@
+import 'dotenv/config';
+import request from 'supertest'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+
+import { setupContainer } from './container.mjs'
+
+let container, app, id_testuser
+
+const username = 'testuser'
+
+beforeAll(async () => {
+
+  vi.stubEnv('DB_PORT', 5432 + Number(process.env.VITEST_WORKER_ID));
+
+  container = await setupContainer()
+  
+  // Import the app AFTER the container is up and env vars are set
+  const mod = await import('../src/express.mjs');
+  app = mod.app;
+
+  const res = await request(app).post('/api/users').send({ username: username })
+  id_testuser = res.body._id
+
+}, 60_000)
+
+afterAll(async () => {
+  if (container) {
+    await container.stop();
+  }
+  vi.unstubAllEnvs()
+})
+
+describe('/api/users/:id/logs', () => {
+  it('Get logs for user with no exercises', async () => {
+    const res = await request(app).get(`/api/users/${id_testuser}/logs`)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toEqual({
+      "_id": id_testuser,
+      "username": username,
+      "count": 0,
+      "log": []
+    })
+  })
+
+  
+
+})
