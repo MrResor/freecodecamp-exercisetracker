4b31b38d52da485bc55aa9047444aa04f787fb8e
Mr.Resor <jakusienko@gmail.com>
2025-06-05 18:17:44 +0000

    add testcases for different cases of missing data

--- __tests__/exercises.test.mjs
+++ __tests__/exercises.test.mjs
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { describe, it, expect, beforeAll } from 'vitest'
 
 import { setupContainer } from './container.mjs'
+import { add } from 'winston'
 
 let container, app, id_testuser
 
@@ -45,6 +46,104 @@ describe('/api/users/:id/exercises', () => {
       date: 'Sun Oct 01 2023',
       duration: 30,
       description: 'test exercise'
+    })
+  })
+  
+  it('add exercise without any data', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({})
+    
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({
+      error: 'Description and duration are required'
+    })
+  })
+
+  it('add exercise without description and duration', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      date: '2023-10-01'
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({
+      error: 'Description and duration are required'
+    })
+  })
+
+  it('add exercise without duration and date', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      description: 'test exercise without duration and date'
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({
+      error: 'Description and duration are required'
+    })
+  })
+
+  it('add exercise without description and date', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      duration: 45
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({
+      error: 'Description and duration are required'
+    })
+  })
+
+  it('add exercise without duration', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      description: 'test exercise without duration',
+      date: '2023-10-02'
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({
+      error: 'Description and duration are required'
+    })
+  })
+
+  it('add exercise without description and date', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      duration: 60
+    })
+
+    expect(res.statusCode).toBe(400)
+    expect(res.body).toEqual({
+      error: 'Description and duration are required'
+    })
+  })
+
+  it('add exercise without date', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      description: 'test exercise without date',
+      duration: 60
+    })
+
+    expect(res.statusCode).toBe(201)
+    expect(res.body).toEqual({
+      _id: id_testuser,
+      username: username,
+      date: new Date().toDateString(),
+      duration: 60,
+      description: 'test exercise without date'
+    })
+  })
+
+  // add exercise without date
+  it('add exercise without date', async () => {
+    const res = await request(app).post(`/api/users/${id_testuser}/exercises`).send({
+      description: 'test exercise without date',
+      duration: 60
+    })
+
+    expect(res.statusCode).toBe(201)
+    expect(res.body).toEqual({
+      _id: id_testuser,
+      username: username,
+      date: new Date().toDateString(),
+      duration: 60,
+      description: 'test exercise without date'
     })
   })
 })
